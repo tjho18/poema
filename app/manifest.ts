56419123dc33aba42583d321f9f7e7a4c60a639b
TJ Ho <tjho18@github.com>
2026-04-24 17:09:02 -0700

    Fix manifest icon purpose type (maskable, not 'any maskable')

--- app/manifest.ts
+++ app/manifest.ts
@@ -21,7 +21,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src:     '/api/icon/512',
         sizes:   '512x512',
         type:    'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
     ],
   }
